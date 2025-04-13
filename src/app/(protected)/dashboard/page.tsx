67402169e7f255a2adca5a0f8c582ae39c101649
TkWarrior <talibulhaque369@gmail.com>
2025-04-13 12:32:13 +0530

    added changes in dashboardpage

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -1,18 +1,39 @@
 "use client"
 import { SignedIn, SignedOut, RedirectToSignIn } from "@clerk/nextjs";
 import { useUser } from "@clerk/nextjs";
-const  DashboardPage = () => {
-  const {user} = useUser();
+import { ExternalLink, Github } from "lucide-react";
+import Link from "next/link";
+import React from "react";
+const DashboardPage = () => {
+  const { project } = useProject();
   return (
     <>
       <SignedIn>
-        <div>{user?.firstName}</div>
+        <div>
+          <div className="flex items-center justify-between flex=wrap gap-y-4">
+            <div className="w-fit rounded-md bg-primary px-4 py-3">
+              <div className="flex items-center">
+                <Github/>
+                <div className="ml-2">
+                  <p className="text-sm font-medium text-white">
+                    <Link href={project?.githubUrl ?? ""} className="inline-flex items-center text-white/80 hover:underline">
+                      {project?.githubUrl}
+                      <ExternalLink className="ml-1 size-4" />
+                    </Link>
+                    
+                  </p>
+                </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        
       </SignedIn>
       <SignedOut>
         <RedirectToSignIn />
       </SignedOut>
     </>
   );
-}
+};
 
 export default DashboardPage
